8f31714132cba30599a1472705520ae70fb80b3c
Rayyan <xlucifer65@gmail.com>
2024-11-04 16:52:40 +0530

    fix:changes

---
+++ app/Dashboard/page.jsx
@@ -0,0 +1,19 @@
+"use client"
+import SearchCard from '@/app/components/dashboard/SearchCard';
+import SignInPage from '../auth/signin';
+
+
+export default function Dashboard() {
+ 
+
+  return (
+    <>
+      <div className="flex flex-row p-1">
+        {/* <SignInPage /> */}
+        
+      </div>
+      <SearchCard />
+      
+    </>
+  );
+}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Page: React.FC = () => {
-  return <div>Hello, Next.js!</div>;
-};
-
-export default Page;
