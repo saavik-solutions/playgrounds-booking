520ba825d81594cfcfffcbd7f6a57ca11b70f9c3
srikar kudurmalla <srikar2985969c@gmail.com>
2024-11-30 02:07:32 +0530

    folder rename

--- v2/src/middlewares/errorHandler.ts
+++
@@ -1,8 +0,0 @@
-// src/middlewares/errorHandler.ts
-
-import { Request, Response, NextFunction } from 'express';
-
-export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  console.error('Unhandled error:', err);
-  res.status(500).json({ error: 'An unexpected error occurred' });
-};
--- v2/src/models/prismaClient.ts
+++
@@ -1,6 +0,0 @@
-// src/models/prismaClient.ts
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-export default prisma;
