4c76c1910e5bbcec5893f7603abd247b53055323
srikar kudurmalla <srikar2985969c@gmail.com>
2024-12-10 10:49:16 +0530

    otp ui integration

---
+++ client/app/(auth)/mobile-otp/page.tsx
@@ -0,0 +1,14 @@
+import PhoneOtpFrom from '@/features/auth/components/phoneLogin'
+import React from 'react'
+
+type Props = {}
+
+const MobileOtp = (props: Props) => {
+  return (
+      <div>
+          <PhoneOtpFrom/>
+    </div>
+  )
+}
+
+export default MobileOtp;
---
+++ client/features/auth/components/phoneLogin.tsx
@@ -0,0 +1,42 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import React, { useState } from 'react'
+
+type Props = {}
+
+const PhoneOtpFrom = (props: Props) => {
+    const [phoneNumber, setPhoneNumber] = useState()
+    const handlePhoneNumber = () => {
+        
+    }
+  return (
+      <div className='space-y-4'>
+          <form onSubmit={() => { }}>
+              <div className="space-y-2">
+                  <Label
+                      className='font-bold text-lg ml-1'
+                      htmlFor='phone'>Mobile Number
+                  </Label>
+                  <Input
+                      id='phone'
+                      type='tel'
+                  value={phoneNumber}
+                  onChange={handlePhoneNumber}
+                  placeholder='Enter Phone Number'
+              />
+                   <Button
+          type="submit"
+          className="w-full bg-gradient-to-r from-blue-600 to-blue-700 hover:from-blue-700 hover:to-blue-800"
+        variant={'default'}
+      >
+       
+        Submit
+      </Button>
+              </div>
+          </form>
+    </div>
+  )
+}
+export default PhoneOtpFrom;
